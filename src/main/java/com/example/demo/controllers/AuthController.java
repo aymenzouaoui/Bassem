0d71c612a1584549ad96a9299fc7fdf9077a4964
aymenzouaoui <aymen.zouaoui@esprit.tn>
2023-12-25 03:41:24 +0100

    Addd AuthController

--- src/main/java/com/example/demo/controllers/AuthController.java
+++ src/main/java/com/example/demo/controllers/AuthController.java
@@ -1,10 +1,37 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.User;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+    @Autowired
+    private UserService userService;
 
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        // Perform validation if needed
+
+        userService.registerUser(user);
+
+        return ResponseEntity.ok("User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestParam String cin, @RequestParam String password) {
+        // Perform validation if needed
+
+        User user = userService.loginUser(cin, password);
+
+        if (user != null) {
+            return ResponseEntity.ok("Login successful");
+        } else {
+            return ResponseEntity.badRequest().body("Invalid credentials");
+        }
+    }
 }
